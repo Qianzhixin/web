3df58377bcbcf47ef467b0dca0b50d43fc451bed
KylezhTao <1042032437@qq.com>
2021-10-16 20:48:19 +0800

    外规表字段类型、长度修改；初始外规入库脚本

--- backend/src/main/resources/InternetFacingPlus.sql
+++ backend/src/main/resources/InternetFacingPlus.sql
@@ -25,17 +25,17 @@ DROP TABLE IF EXISTS `external_regulation`;
 CREATE TABLE external_regulation
 (
     id INT UNIQUE NOT NULL AUTO_INCREMENT COMMENT '主键',
-    title VARCHAR(30) NOT NULL COMMENT '法规标题',
-    number VARCHAR(20) COMMENT '法规文号',
-    type VARCHAR(20) NOT NULL COMMENT '外规类别',
-    publishing_department VARCHAR(20) NOT NULL COMMENT '发文部门',
-    effectiveness_level INT NOT NULL COMMENT '效力等级',
-    release_date DATETIME NOT NULL COMMENT '发布日期',
-    implementation_date DATETIME NOT NULL COMMENT '实施日期',
-    interpretation_department VARCHAR(20) COMMENT '解读部门',
+    title VARCHAR(255) NOT NULL COMMENT '法规标题',
+    number VARCHAR(30) COMMENT '法规文号',
+    type VARCHAR(30) NOT NULL COMMENT '外规类别',
+    publishing_department VARCHAR(50) NOT NULL COMMENT '发文部门',
+    effectiveness_level VARCHAR(30) NOT NULL COMMENT '效力等级',
+    release_date VARCHAR(30) NOT NULL COMMENT '发布日期',
+    implementation_date VARCHAR(30) NOT NULL COMMENT '实施日期',
+    interpretation_department VARCHAR(50) COMMENT '解读部门',
     input_person_id INT NOT NULL COMMENT '录入人id',
     input_date DATETIME NOT NULL COMMENT '录入时间',
-    text_path VARCHAR(260) NOT NULL COMMENT '正文文件路径',
+    text_path VARCHAR(255) NOT NULL COMMENT '正文文件路径',
     state INT NOT NULL COMMENT '法规是否发布,0为未发布,1为发布',
     CONSTRAINT external_regulation_pk
         PRIMARY KEY (id),
@@ -48,5 +48,5 @@ INSERT INTO `user`
 VALUES (1,'root','111111','张三');
 
 INSERT INTO `external_regulation`
-VALUES (1,'测试法规1','','测试类别1','部门1',1,'2021-04-29 18:37:10','2021-04-29 18:37:10','部门2',1,'2021-04-28 18:37:10','F:\\warehouse\\SePractice\\testdir\\测试法规1.doc',0),
-       (2,'测试法规2','','测试类别2','部门2',1,'2021-04-29 18:37:10','2021-04-29 18:37:10','部门1',1,'2021-04-28 18:37:10','F:\\warehouse\\SePractice\\testdir\\测试法规2.doc',1);
+VALUES (1,'测试法规1','','测试类别1','部门1','1','2021-04-29 18:37:10','2021-04-29 18:37:10','部门2',1,'2021-04-28 18:37:10','F:\\warehouse\\SePractice\\testdir\\测试法规1.doc',0),
+       (2,'测试法规2','','测试类别2','部门2','1','2021-04-29 18:37:10','2021-04-29 18:37:10','部门1',1,'2021-04-28 18:37:10','F:\\warehouse\\SePractice\\testdir\\测试法规2.doc',1);
